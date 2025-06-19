6832ea44938e1d553b1ef9c6cd2c17c012c73471
Fausto De Leon <fdeleon@colgate.edu>
2025-06-19 03:14:00 -0400

    Minor performance tweaks

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
+var compression = require('compression')
+
 const app = express();
 const port = process.env.PORT
 app.use(express.static('public'))
+app.use(compression({ level: 9 }))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
